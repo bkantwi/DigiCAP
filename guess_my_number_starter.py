c5d7fd01972e412da5e2373737e67afc11250cf0
Bernard Antwi <bernardantwi2010@gmail.com>
2021-08-31 18:33:30 +0000

    first commit

--- guess_my_number_starter.py
+++ guess_my_number_starter.py
@@ -4,15 +4,17 @@ File: guessmynumber.py
 This program checks if a user's guess of a number matches that guessed by the computer
 """
 
+# import random library
 import random
-# use code below  to generate a random integer between 30 and 50 for example
 number = (random.randint(1, 99))
-# print(number)
-# ********************************** YOUR CODE GOES BELOW HERE *********************************************************
 
+# setting guess count to 0
 user_guess = 0
+
+# User input guess number
 guessed_number = int(input("Guess an integer between 1 and 99: "))
 
+# loop to check if users guess is greater or less than the random number
 while guessed_number != number:
     if guessed_number < number:
         print("Kindly guess again. Guess is too low.")
@@ -23,4 +25,5 @@ while guessed_number != number:
         guessed_number = int(input("Guess an integer between 1 and 99: "))
         user_guess += 1
 
+# Print if user has got the correct guess
 print("Congratulations on your correct guess")
