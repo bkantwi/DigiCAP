953b5fc428b0f9d1b20f72f237fdef9b004f4b81
Bernard Antwi <bernardantwi2010@gmail.com>
2021-09-01 17:36:35 +0000

    Code coommented

--- khansole_academy_starter.py
+++ khansole_academy_starter.py
@@ -6,27 +6,44 @@ randomly generating integers between 10 and 99 inclusive. The program returns fe
 
 """
 
+# Import the random library
 import random
-# use code below  to generate a random integer between 30 and 50 for example
-# print(random.randint(10, 99))
+
+# Initialise correct answers count to zero
 correct_answers = 0
-# ********************************** YOUR CODE GOES BELOW HERE *********************************************************
+
+# For fun. Just to print name in the end
+user_name = input("Kindly input your name to begin: ")
+
+# Setting the loop to infinity
 while True:
+    # Setting the range for the random numbers
     a = random.randint(10, 99)
     b = random.randint(10, 99)
+    # Storing the answer to the question
+    answer = a + b
 
-    print("What is " + str(a) + " plus " + str(b) + "?")
+    # Presenting the question to the user
+    print(f"What is {a} + {b}")
+
+    # Taking the user input
     user_answer = int(input())
 
-    if user_answer == (a+b):
-        print("Correct")
+    # validate the users input if it is right or wrong
+    if user_answer == answer:
+        print(f"Your answer is {user_answer}")
+        # To count the correct answers
         correct_answers += 1
+        print(f"Correct!!! You have {correct_answers} corrects in a row")
     else:
-        print("Incorrect. The answer is " + str(a+b))
-        correct_answers = correct_answers -correct_answers
+        print(f"Your answer is {user_answer}")
+        # To make the correct answers zero when one wrong is achieved
+        correct_answers = correct_answers - correct_answers
+        print(f"Incorrect. You have {correct_answers} corrects in a row")
 
-    print("Player has " + str(correct_answers) + " points")
-
+    # Validate if the correct answers in a row is 3 to end the game
     if correct_answers == 3:
-        print("Congrats. You have mastered addition")
+        print(f"Congrats {user_name}!!! You have mastered addition")
+
+        # To escape the loop
         break
